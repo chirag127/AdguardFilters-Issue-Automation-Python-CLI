0ad8c883fb17d20e8bdb871bb3fb3b3aeaf8daa7
chirag <whyiswhen@gmail.com>
2021-12-08 08:48:48 +0530

    AdguardFilters/AR:

--- AdguardFilters/AR.py
+++ AdguardFilters/AR.py
@@ -49,25 +49,23 @@ def main():
 
     pyautogui.hotkey('ctrl', 'w')
 
-    webbrowser.open("https://github.com/" + Create_new_issue_Account + "/issues/new?template=" + Create_new_issue_template + "&title=" + site_domain)
-
+    webbrowser.open("https://github.com/" + Create_new_issue_Account +
+                    "/issues/new?template=" + Create_new_issue_template + "&title=" + site_domain)
 
     sleep(3)
 
-
     while True:
 
-            image_url = clipboard.paste()
+        image_url = clipboard.paste()
 
-            if "imgur" not in image_url:
+        if "imgur" not in image_url:
 
-                sleep(1)
-                print("waiting for the image to be uploaded") 
+            sleep(1)
+            print("waiting for the image to be uploaded")
 
+        else:
 
-            else:
-
-                break  
+            break
 
     fill_product_normal()
 
