946dcc41e6694c2186a5112ea74244bebb80763b
chirag <whyiswhen@gmail.com>
2021-12-09 20:47:58 +0530

    AdguardFilters/AR:

--- AdguardFilters/AR.py
+++ AdguardFilters/AR.py
@@ -14,9 +14,11 @@ if __name__ == "__main__":
     while True:
 
         if keyboard.is_pressed('ctrl + q'):
+
             create_issue("bug_report.yml", Create_new_issue_Account)
 
         if keyboard.is_pressed('ctrl + shift + m'):
+            
             create_issue("bug_report_NSFW.yml", Create_new_issue_Account)
 
         else:
