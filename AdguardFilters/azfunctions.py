d1cedf95e1b2bea9dda12e0fcbafab80a8a4d756
chirag <whyiswhen@gmail.com>
2021-12-14 17:24:54 +0530

    AdguardFilters/azfunctions:

--- AdguardFilters/azfunctions.py
+++ AdguardFilters/azfunctions.py
@@ -106,14 +106,13 @@ def create_issue(Create_new_issue_template, Create_new_issue_Account):
 
     pyautogui.hotkey('ctrl', 'a')
 
-    pyperclip.copy(f"""<details><summary>Screenshots:</summary>
+    pyperclip.copy(f"""> Please tell me user filters if the issue is out of scope for Adguard filters.
+    
 
-
+<details><summary>Screenshots:</summary>
 
 
 [screenshot]({image_url})
-
-
 
 
 </details><br/>""")
