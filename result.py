1567c62aee5e8381d913ecfe03d26ae791d284ed
chirag <whyiswhen@gmail.com>
2022-05-14 19:29:51 +0530

    result:

--- result.py
+++ result.py
@@ -11,32 +11,30 @@ import pyperclip
 # Physical: {X=923,Y=614};
 def main(roll_number):
 
-
-
     url = "http://jabinresults.eadminaargees.com/"
 
     webbrowser.open(url)
 
     pyperclip.copy(roll_number)
 
-    sleep(1)
+    sleep(2)
 
     pyautogui.click(x=562, y=448)
 
     sleep(0.1)
-
+    
     pyautogui.click(x=618, y=724)
 
     sleep(0.1)
-
+    
     pyautogui.click(x=516, y=493)
 
     sleep(0.1)
-
-    pyperclip.paste()
+    
+    pyautogui.hotkey('ctrl', 'v')
 
     sleep(0.1)
-
+    
     pyautogui.press('enter')
 
     sleep(1)
@@ -44,20 +42,21 @@ def main(roll_number):
     pyautogui.rightClick(x=850, y=739)
 
     sleep(0.1)
-
+    
     pyautogui.click(x=923, y=614)
 
     sleep(0.1)
-
-    pyperclip.paste()
+    
+    pyperclip.hotkey('ctrl', 'c')
 
     sleep(0.1)
-
+    
     pyautogui.press('enter')
 
     sleep(1)
 
 if __name__ == "__main__":
     
-    for i in range(219001,219010):
-        main(i)
+    # for i in range(219001,219010):
+    #     main(i)
+    main(219001)
